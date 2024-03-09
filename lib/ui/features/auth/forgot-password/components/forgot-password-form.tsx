1327c0cd0625ddc0c42277299f50ccf0b5fcd742
Arnau <agomez@mrmilu.com>
2024-03-10 00:24:41 +0100

    feat: forgot password page

--- lib/ui/features/auth/forgot-password/components/forgot-password-form.tsx
+++ lib/ui/features/auth/forgot-password/components/forgot-password-form.tsx
@@ -8,20 +8,16 @@ import { InputFormField } from "@/lib/ui/components/form/form-fields";
 import { Button } from "@/lib/ui/components/shadcn/ui/button";
 import { Form } from "@/lib/ui/components/shadcn/ui/form";
 import Link from "next/link";
-import { cn } from "@/lib/ui/utils/shadcn";
-import { textStyles } from "@/lib/ui/styles/text-styles";
 
 const FormSchema = z.object({
   email: z.string().email(),
-  password: z.string(),
 });
 
-export function LoginForm() {
+export function ForgotPasswordForm() {
   const form = useForm({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   });
 
@@ -31,33 +27,18 @@ export function LoginForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <InputFormField
           label="Correo electrónico"
           name="email"
           placeholder="Tu correo electrónico"
           autoComplete="email"
         />
-        <div className="h-6" />
-        <InputFormField
-          label="Contraseña"
-          name="password"
-          placeholder="Tu contraseña"
-          type="password"
-          autoComplete="current-password"
-        />
-        <div className="h-2" />
-        <p className={cn(textStyles.muted, "text-right hover:underline")}>
-          <Link className="" href="/auth/forgot-password">
-            Olvidaste tu contraseña?
-          </Link>
-        </p>
-        <div className="h-6" />
         <div className="flex space-x-6 justify-between">
           <Button variant="ghost" asChild>
-            <Link href="/auth/register">Nuevo usuario</Link>
+            <Link href="/auth/login">Login</Link>
           </Button>
-          <Button>Login</Button>
+          <Button>Enviar</Button>
         </div>
       </form>
     </Form>
