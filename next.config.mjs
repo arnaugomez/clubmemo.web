87256630bb42df37025ec4a7f86c3712015e6f94
Arnau <agomez@mrmilu.com>
2024-03-10 23:23:12 +0100

    feat: lucia mongodb setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Prevent mongoose error
-  experimental: {
-    esmExternals: "loose",
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   webpack(config) {
-    // Prevent mongoose error
-    config.experiments.topLevelAwait = true;
     // If you're planning to use oslo/password for hashing passwords, mark its dependencies as external to prevent it from getting bundled.
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
