50be5e5044ba04eeadd2f37f90620a26d67bb67a
Arnau <agomez@mrmilu.com>
2024-03-12 13:46:32 +0100

    feat: add route guards

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,7 +1,21 @@
+import { checkSession } from "@/src/check-session";
 import { textStyles } from "@/src/ui/styles/text-styles";
 import { MailCheck } from "lucide-react";
+import { redirect } from "next/navigation";
 
-export default function VerifyEmailPage() {
+async function verifyEmailGuard() {
+  const result = await checkSession();
+  if (!result.session) {
+    redirect("/login");
+  }
+  if (result.user.isEmailVerified) {
+    redirect("/home");
+  }
+}
+
+export default async function VerifyEmailPage() {
+  await verifyEmailGuard();
+  const { user } = await checkSession();
   return (
     <>
       <div className="w-full max-w-prose">
@@ -10,8 +24,8 @@ export default function VerifyEmailPage() {
         <h1 className={textStyles.h2}>¡Ya casi estamos!</h1>
         <div className="h-4"></div>
         <p>
-          Te hemos enviado un correo electrónico a TODO con instrucciones para
-          verificar tu cuenta.
+          Te hemos enviado un correo electrónico a {user?.email} con
+          instrucciones para verificar tu cuenta.
         </p>
       </div>
     </>
