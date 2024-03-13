422ea23fbca8596f0e04bb74aba5f12535ad1b8f
Arnau <agomez@mrmilu.com>
2024-03-13 21:18:30 +0100

    feat: forgot password action

--- src/ui/features/auth/forgot-password/components/forgot-password-form.tsx
+++ src/ui/features/auth/forgot-password/components/forgot-password-form.tsx
@@ -8,6 +8,9 @@ import { InputFormField } from "@/src/ui/components/form/form-fields";
 import { Button } from "@/src/ui/components/shadcn/ui/button";
 import { Form } from "@/src/ui/components/shadcn/ui/form";
 import Link from "next/link";
+import { forgotPasswordAction } from "../actions/forgot-password-action";
+import { FormGlobalErrorMessage } from "@/src/ui/components/form/form-global-error-message";
+import { FormResponseHandler } from "@/src/ui/view-models/server-form-errors";
 
 const FormSchema = z.object({
   email: z.string().email(),
@@ -21,8 +24,15 @@ export function ForgotPasswordForm() {
     },
   });
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data);
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      const response = await forgotPasswordAction(data.email);
+      const handler = new FormResponseHandler(response, form);
+      handler.setErrors();
+    } catch (error) {
+      console.error(error);
+      FormResponseHandler.setGlobalError(form);
+    }
   }
 
   return (
@@ -34,6 +44,8 @@ export function ForgotPasswordForm() {
           placeholder="Tu correo electrónico"
           autoComplete="email"
         />
+        <FormGlobalErrorMessage />
+        <div className="h-6" />
         <div className="flex space-x-6 justify-between">
           <Button variant="ghost" asChild>
             <Link href="/auth/login">Login</Link>
