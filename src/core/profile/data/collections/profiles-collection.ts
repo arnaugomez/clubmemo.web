d3ff0882fab07c5af3ed60e677d98683bd580198
Arnau <agomez@mrmilu.com>
2024-03-14 08:51:08 +0100

    feat: profile repository

--- src/core/profile/data/collections/profiles-collection.ts
+++ src/core/profile/data/collections/profiles-collection.ts
@@ -1,8 +1,9 @@
 import { collection } from "@/src/core/app/utils/mongo";
-import { ObjectId } from "mongodb";
+import { ObjectId, WithId } from "mongodb";
+import { ProfileModel } from "../../domain/models/profile-model";
 
 export interface ProfileDoc {
-  userId?: ObjectId;
+  userId: ObjectId;
   displayName?: string;
   handle?: string;
   bio?: string;
@@ -11,3 +12,19 @@ export interface ProfileDoc {
 }
 
 export const profilesCollection = collection<ProfileDoc>("profiles");
+
+export class ProfileDocTransformer {
+  constructor(private readonly doc: WithId<ProfileDoc>) {}
+
+  toDomain(): ProfileModel {
+    return new ProfileModel({
+      id: this.doc._id,
+      userId: this.doc.userId,
+      displayName: this.doc.displayName,
+      handle: this.doc.handle,
+      bio: this.doc.bio,
+      picture: this.doc.picture,
+      backgroundPicture: this.doc.backgroundPicture,
+    });
+  }
+}
