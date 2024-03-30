edb15b47e0de6939751de5f83262235b0576c472
Arnau <agomez@mrmilu.com>
2024-03-30 10:23:43 +0100

    feat: course detail page layout

--- src/app/(admin-layout)/courses/detail/[id]/page.tsx
+++ src/app/(admin-layout)/courses/detail/[id]/page.tsx
@@ -10,7 +10,6 @@ export default function CourseDetailPage({
     <div className="size-full overflow-y-auto md:overflow-y-hidden md:flex md:divide-x-[1px] divide-slate-200">
       <div className="flex-none w-full md:max-w-sm lg:max-w-md xl:max-w-lg md:overflow-y-auto">
         <CourseDetailMainSection id={id} />
-        course {id}
       </div>
       <div className="flex-1 bg-slate-100 md:overflow-y-auto"></div>
     </div>
---
+++ src/ui/features/courses/detail/components/course-detail-actions-section.tsx
@@ -0,0 +1,18 @@
+import { CourseModel } from "@/src/core/courses/domain/models/course-model";
+import { CourseDetailActionsDropdown } from "./course-detail-actions-dropdown";
+import { CourseDetailEditSection } from "./course-detail-edit-section";
+
+interface CourseDetailActionsSectionProps {
+  course: CourseModel;
+}
+
+export async function CourseDetailActionsSection({
+  course,
+}: CourseDetailActionsSectionProps) {
+  return (
+    <div className="absolute bottom-4 right-4 flex space-x-4">
+      {course.canEdit && <CourseDetailEditSection courseData={course.data} />}
+      <CourseDetailActionsDropdown courseData={course.data} />
+    </div>
+  );
+}
