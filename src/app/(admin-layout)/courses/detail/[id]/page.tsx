817b03c96bef2c6d722aebf440c3bedd071b9561
Arnau <agomez@mrmilu.com>
2024-03-30 10:30:48 +0100

    feat: course detail learning cards section

--- src/app/(admin-layout)/courses/detail/[id]/page.tsx
+++ src/app/(admin-layout)/courses/detail/[id]/page.tsx
@@ -1,4 +1,8 @@
 import { CourseDetailMainSection } from "@/src/ui/features/courses/detail/components/course-detail-main-section";
+import { textStyles } from "@/src/ui/styles/text-styles";
+import { cn } from "@/src/ui/utils/shadcn";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function CourseDetailPage({
   params: { id },
@@ -11,7 +15,25 @@ export default function CourseDetailPage({
       <div className="flex-none w-full md:max-w-sm lg:max-w-md xl:max-w-lg md:overflow-y-auto">
         <CourseDetailMainSection id={id} />
       </div>
-      <div className="flex-1 bg-slate-100 md:overflow-y-auto"></div>
+      {/* TODO: create course detail cards section component */}
+      <div className="flex-1 bg-slate-100 md:overflow-y-auto">
+        <div className="h-16"></div>
+        <div className="px-4">
+          <div className="mx-auto max-w-prose">
+            <h2 className={cn(textStyles.h2)}>Tarjetas de aprendizaje</h2>
+
+            {/* TODO: Extract arrow link component. link addrress is missing. */}
+            <Link
+              href="/"
+              className={cn(textStyles.mutedLink, "space-x-1 pt-1")}
+            >
+              <span>Ver todas</span>
+
+              <ArrowRight size={16} className="inline" />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
