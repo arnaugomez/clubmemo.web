1343e4c0eaeb49433858fb51f4f04c2df3083af9
Arnau <agomez@mrmilu.com>
2024-04-01 10:55:30 +0200

    fix: move enroll button to more important section

--- src/ui/features/courses/detail/components/course-detail-actions-section.tsx
+++ src/ui/features/courses/detail/components/course-detail-actions-section.tsx
@@ -1,7 +1,6 @@
 import { CourseModel } from "@/src/core/courses/domain/models/course-model";
-import { CourseDetailActionsDropdown } from "./course-detail-actions-dropdown";
 import { CourseDetailEditSection } from "../../edit/components/edit-course-section";
-import { CourseDetailEnrollSection } from "../../enroll/components/course-detail-enroll-section";
+import { CourseDetailActionsDropdown } from "./course-detail-actions-dropdown";
 
 interface CourseDetailActionsSectionProps {
   course: CourseModel;
@@ -12,9 +11,6 @@ export async function CourseDetailActionsSection({
 }: CourseDetailActionsSectionProps) {
   return (
     <div className="absolute bottom-4 right-4 flex space-x-2">
-      {!course.isOwner && (
-        <CourseDetailEnrollSection courseData={course.data} />
-      )}
       {course.canEdit && <CourseDetailEditSection courseData={course.data} />}
       <CourseDetailActionsDropdown courseData={course.data} />
     </div>
