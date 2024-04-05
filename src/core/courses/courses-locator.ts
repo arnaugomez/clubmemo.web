4a36c2cf9d7bb2a2ebfabbccbd7d77110e46efbe
Arnau <agomez@mrmilu.com>
2024-04-05 18:01:28 +0200

    feat(courses): copy course

--- src/core/courses/courses-locator.ts
+++ src/core/courses/courses-locator.ts
@@ -1,8 +1,10 @@
 import { Lazy, locator } from "../app/locator";
+import type { CopyCourseUseCase } from "./domain/use-cases/copy-course-use-case";
 import type { UpdateCourseUseCase } from "./domain/use-cases/update-course-use-case";
 
 interface CoursesLocator {
   UpdateCourseUseCase: Lazy<UpdateCourseUseCase>;
+  CopyCourseUseCase: Lazy<CopyCourseUseCase>;
 }
 
 export const coursesLocator: CoursesLocator = {
@@ -11,6 +13,13 @@ export const coursesLocator: CoursesLocator = {
     return new file.UpdateCourseUseCase(
       await locator.TagsRepository(),
       await locator.CoursesRepository(),
+    );
+  },
+  CopyCourseUseCase: async () => {
+    const file = await import("./domain/use-cases/copy-course-use-case");
+    return new file.CopyCourseUseCase(
+      await locator.CoursesRepository(),
+      await locator.NotesRepository(),
     );
   },
 };
