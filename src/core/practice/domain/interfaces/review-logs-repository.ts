cab02542424aec8b78f94cc89ec09c4c27adc07a
Arnau <agomez@mrmilu.com>
2024-04-13 13:50:13 +0200

    chore: format

--- src/core/practice/domain/interfaces/review-logs-repository.ts
+++ src/core/practice/domain/interfaces/review-logs-repository.ts
@@ -3,7 +3,5 @@ import { ReviewLogModel } from "../models/review-log-model";
 export interface ReviewLogsRepository {
   create(input: ReviewLogModel): Promise<ReviewLogModel>;
 
-  getReviewsOfNewCardsCount(
-    courseEnrollmentId: string
-  ): Promise<number>;
+  getReviewsOfNewCardsCount(courseEnrollmentId: string): Promise<number>;
 }
