4d178d87517815629c8c8dd8db0643984b3be2c6
Arnau <agomez@mrmilu.com>
2024-04-15 19:39:04 +0200

    feat(courses): interesting courses format errors

--- src/app/(login-guard)/(navbar-layout)/home/page.tsx
+++ src/app/(login-guard)/(navbar-layout)/home/page.tsx
@@ -3,8 +3,6 @@ import { InterestingCoursesSection } from "@/src/ui/features/courses/interesting
 import { KeepLearningSection } from "@/src/ui/features/courses/keep-learning/components/keep-learning-section";
 import { fetchHasCourses } from "@/src/ui/features/courses/my-courses/fetch/fetch-has-courses";
 import { HomeGreeting } from "@/src/ui/features/home/components/home-greeting";
-import { textStyles } from "@/src/ui/styles/text-styles";
-import { cn } from "@/src/ui/utils/shadcn";
 
 export default async function HomePage() {
   const hasCourses = await fetchHasCourses();
@@ -23,14 +21,6 @@ function HomePageContent() {
   return (
     <>
       <KeepLearningSection />
-      <div className="h-12" />
-      <section className="px-4">
-        <div className="mx-auto max-w-prose">
-          <h2 className={cn(textStyles.h3, "mx-auto max-w-prose")}>
-            Tu progreso
-          </h2>
-        </div>
-      </section>
       <div className="h-12" />
       <InterestingCoursesSection />
     </>
