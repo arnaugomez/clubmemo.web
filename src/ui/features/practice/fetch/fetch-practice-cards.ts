b2124d15106f01a75b0d1185ac19525e702c067a
Arnau <agomez@mrmilu.com>
2024-04-16 18:50:21 +0200

    feat: get next practice cards

---
+++ src/ui/features/practice/fetch/fetch-practice-cards.ts
@@ -0,0 +1,42 @@
+import { locator } from "@/src/core/common/locator";
+import { shuffle } from "@/src/core/common/utils/array";
+import { CourseEnrollmentModel } from "@/src/core/courses/domain/models/course-enrollment-model";
+import { CourseModel } from "@/src/core/courses/domain/models/course-model";
+
+interface FetchPracticeCardsInput {
+  course: CourseModel;
+  enrollment: CourseEnrollmentModel;
+}
+export async function fetchPracticeCards({
+  course,
+  enrollment,
+}: FetchPracticeCardsInput) {
+  const courseEnrollmentId = enrollment.id;
+
+  const reviewLogsRepository = await locator.ReviewLogsRepository();
+  const practiceCardsRepository = await locator.PracticeCardsRepository();
+
+  const newReviewsCountPromise =
+    reviewLogsRepository.getReviewsOfNewCardsCount(courseEnrollmentId);
+  const cardsPerSessionCount = enrollment.config.cardsPerSessionCount;
+  const newCardsPromise = practiceCardsRepository.getUnpracticed({
+    courseEnrollmentId,
+    courseId: course.id,
+    limit: cardsPerSessionCount,
+  });
+  const dueCardsPromise = practiceCardsRepository.getDue({
+    courseEnrollmentId,
+    limit: cardsPerSessionCount,
+  });
+
+  const newReviewsCount = await newReviewsCountPromise;
+  const cardsToLearnCount = Math.max(
+    0,
+    enrollment.config.dailyNewCardsCount - newReviewsCount,
+  );
+  const newCards = cardsToLearnCount > 0 ? await newCardsPromise : [];
+  const newCardsToLearn = newCards.slice(0, cardsToLearnCount);
+  const dueCards = await dueCardsPromise;
+  const cards = shuffle([...newCardsToLearn, ...dueCards]);
+  return cards.slice(0, cardsPerSessionCount);
+}
