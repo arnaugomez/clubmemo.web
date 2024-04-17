208551d225304e2c29ec04f62f05ff13564f4543
Arnau <agomez@mrmilu.com>
2024-04-17 13:14:50 +0200

    feat: make the front field a wysiwyg editor

--- src/ui/components/input/wysiwyg-input.tsx
+++ src/ui/components/input/wysiwyg-input.tsx
@@ -1,6 +1,7 @@
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { cn } from "../../utils/shadcn";
 import "./wysiwyg-input.css";
 
 interface WysiwygInputProps {
@@ -10,6 +11,7 @@ interface WysiwygInputProps {
 
   value: string;
   onChange: (content: string) => void;
+  isSmall?: boolean;
 }
 
 export function WysiwygInput({
@@ -18,8 +20,17 @@ export function WysiwygInput({
   placeholder,
   value,
   onChange,
+  isSmall,
 }: WysiwygInputProps) {
   const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class: cn(
+          !isSmall && "min-h-[80px]",
+          "w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        ),
+      },
+    },
     extensions: [StarterKit, Placeholder.configure({ placeholder })],
     content: value,
     onUpdate({ editor }) {
@@ -27,5 +38,7 @@ export function WysiwygInput({
     },
   });
 
-  return <EditorContent name={name} id={id} editor={editor} />;
+  return (
+    <EditorContent className="wysiwyg" name={name} id={id} editor={editor} />
+  );
 }
