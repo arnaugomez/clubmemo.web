af0353dc0efe52ed69f7c11902bf5fee35e05c43
Arnau <agomez@mrmilu.com>
2024-04-24 23:05:38 +0200

    feat: simplify layout and improve performance

--- src/app/(admin-layout)/layout.tsx
+++ src/app/(admin-layout)/layout.tsx
@@ -6,11 +6,9 @@ export default async function NavbarLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="relative h-dvh">
-      <div className="absolute inset-0 flex flex-col">
-        <Navbar />
-        <div className="relative min-h-0 flex-1">{children}</div>
-      </div>
+    <main className="flex h-dvh flex-col">
+      <Navbar />
+      <div className="relative min-h-0 flex-1">{children}</div>
     </main>
   );
 }
