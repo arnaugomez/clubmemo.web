a6bc773cbf13b6b9aed062e35341937e0a5892f3
Arnau <agomez@mrmilu.com>
2024-04-24 23:03:47 +0200

    fix: error with course page layout on mobile

--- src/app/(admin-layout)/layout.tsx
+++ src/app/(admin-layout)/layout.tsx
@@ -6,9 +6,11 @@ export default async function NavbarLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-vh flex flex-col">
-      <Navbar />
-      <main className="min-h-0 flex-1">{children}</main>
-    </div>
+    <main className="relative h-dvh">
+      <div className="absolute inset-0 flex flex-col">
+        <Navbar />
+        <div className="relative min-h-0 flex-1">{children}</div>
+      </div>
+    </main>
   );
 }
