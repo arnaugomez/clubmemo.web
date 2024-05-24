9b8e56a77194900660da30c2f2c69fed1a86a629
Arnau <agomez@mrmilu.com>
2024-05-24 20:22:04 +0200

    fix: validate input data of server action

--- src/auth/ui/verify-email/actions/verify-email-action.ts
+++ src/auth/ui/verify-email/actions/verify-email-action.ts
@@ -2,36 +2,43 @@
 
 import { fetchSession } from "@/src/auth/ui/fetch/fetch-session";
 import { locator } from "@/src/common/locator";
+import { ActionErrorHandler } from "@/src/common/ui/actions/action-error-handler";
 import { ActionResponse } from "@/src/common/ui/models/server-form-errors";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import type { VerifyEmailActionModel } from "../schemas/verify-email-action-schema";
+import { VerifyEmailActionSchema } from "../schemas/verify-email-action-schema";
 
-interface VerifyEmailActionViewModel {
-  code: string;
-}
+export async function verifyEmailAction(input: VerifyEmailActionModel) {
+  try {
+    const { code } = VerifyEmailActionSchema.parse(input);
+    const { user } = await fetchSession();
+    if (!user) {
+      return ActionResponse.formGlobalError("sessionExpired");
+    }
 
-export async function verifyEmailAction({ code }: VerifyEmailActionViewModel) {
-  const { user } = await fetchSession();
-  if (!user) {
-    return ActionResponse.formGlobalError("sessionExpired");
+    const emailVerificationCodesRepository =
+      await locator.EmailVerificationCodesRepository();
+    const isValid = await emailVerificationCodesRepository.verify(
+      user.id,
+      code,
+    );
+    if (!isValid) {
+      return ActionResponse.formError("code", {
+        type: "invalidCode",
+        message: "Invalid code",
+      });
+    }
+
+    const sessionCookie = await locator.AuthService().verifyEmail(user.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+  } catch (e) {
+    return ActionErrorHandler.handle(e);
   }
-
-  const emailVerificationCodesRepository =
-    await locator.EmailVerificationCodesRepository();
-  const isValid = await emailVerificationCodesRepository.verify(user.id, code);
-  if (!isValid) {
-    return ActionResponse.formError("code", {
-      type: "invalidCode",
-      message: "Invalid code",
-    });
-  }
-
-  const sessionCookie = await locator.AuthService().verifyEmail(user.id);
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
 
   redirect(`/home`);
 }
