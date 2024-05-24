21de9a4c20d71927c4b7a36e5ea5b870f0c515eb
Arnau <agomez@mrmilu.com>
2024-05-24 21:28:54 +0200

    fix: validate data of server action

--- src/courses/ui/create/actions/create-course-action.ts
+++ src/courses/ui/create/actions/create-course-action.ts
@@ -5,20 +5,22 @@ import { ActionResponse } from "@/src/common/ui/models/server-form-errors";
 import { ProfileDoesNotExistError } from "@/src/profile/domain/errors/profile-errors";
 import { revalidatePath, revalidateTag } from "next/cache";
 import { fetchMyProfile } from "../../../../profile/ui/fetch/fetch-my-profile";
+import type { CreateCourseActionModel } from "../schemas/create-course-action-schema";
+import { CreateCourseActionSchema } from "../schemas/create-course-action-schema";
 
-interface CreateCourseActionModel {
-  name: string;
-}
-
-export async function createCourseAction(data: CreateCourseActionModel) {
+export async function createCourseAction(input: CreateCourseActionModel) {
   try {
+    const { name } = CreateCourseActionSchema.parse(input);
+
     const profile = await fetchMyProfile();
     if (!profile) throw new ProfileDoesNotExistError();
+
     const coursesRepository = await locator.CoursesRepository();
     const course = await coursesRepository.create({
-      name: data.name,
+      name,
       profileId: profile.id,
     });
+
     revalidatePath("/courses");
     revalidatePath("/learn");
     revalidateTag("hasCourses");
