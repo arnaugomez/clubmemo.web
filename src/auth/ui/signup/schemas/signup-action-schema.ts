c91612b4e270a9674a0aec66fcc698eb43c54ec7
Arnau <agomez@mrmilu.com>
2024-05-25 15:42:29 +0200

    feat(auth): check that password is sufficiently secure

--- src/auth/ui/signup/schemas/signup-action-schema.ts
+++ src/auth/ui/signup/schemas/signup-action-schema.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 export const SignupActionSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().email().max(254),
+  password: z
+    .string()
+    .min(8)
+    .max(256)
+    .regex(/[A-Z]/)
+    .regex(/[a-z]/)
+    .regex(/[0-9]/)
+    .regex(/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/),
 });
 
 export type SignupActionModel = z.infer<typeof SignupActionSchema>;
