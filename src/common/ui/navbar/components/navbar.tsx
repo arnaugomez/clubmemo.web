d4d5360b73794a83646a7144a9f2d6fbd458c958
Arnau <agomez@mrmilu.com>
2024-05-26 10:10:58 +0200

    refactor: simplify and reuse components of navbar

--- src/common/ui/navbar/components/navbar.tsx
+++ src/common/ui/navbar/components/navbar.tsx
@@ -1,48 +1,24 @@
-import { fetchSession } from "@/src/auth/ui/fetch/fetch-session";
-import Link from "next/link";
 import { Suspense } from "react";
-import { Button } from "../../components/shadcn/ui/button";
-import { textStyles } from "../../styles/text-styles";
-import { cn } from "../../utils/shadcn";
-import { NavbarLinksSection } from "./navbar-links-section";
+import { NavbarLinks } from "./navbar-links";
 import { NavbarUserSection } from "./navbar-user-section";
+import { NavbarUserSectionLoggedOut } from "./navbar-user-session-logged-out";
+import { NavbarLogo } from "./navgar-logo";
 
 export function Navbar() {
   return (
     <header className="h-16 flex-none">
       <nav className="fixed inset-x-0 top-0 z-10 flex h-16 items-center justify-between border-b-[1px] border-slate-200 bg-white px-6">
-        <span className={cn(textStyles.logo, "text-slate-700")}>
-          <Suspense fallback="clubmemo">
-            <NavbarTitle />
-          </Suspense>
-        </span>
+        <NavbarLogo />
 
         <div className="flex items-center space-x-8">
           <Suspense>
-            <NavbarLinksSection />
+            <NavbarLinks />
           </Suspense>
-          <Suspense>
-            <UserSection />
+          <Suspense fallback={<NavbarUserSectionLoggedOut />}>
+            <NavbarUserSection />
           </Suspense>
         </div>
       </nav>
     </header>
   );
-}
-
-async function NavbarTitle() {
-  const result = await fetchSession();
-  return result.session ? <Link href="/home">clubmemo</Link> : "clubmemo";
-}
-
-async function UserSection() {
-  const result = await fetchSession();
-  if (!result.session || !result.user.isEmailVerified) {
-    return (
-      <Button variant="outline" asChild>
-        <Link href="/auth/login">Login</Link>
-      </Button>
-    );
-  }
-  return <NavbarUserSection />;
 }
