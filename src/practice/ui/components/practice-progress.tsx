f494cd2d80904b5531eeef4696c300c0cd516844
Arnau <agomez@mrmilu.com>
2024-05-26 20:05:37 +0200

    style: format code

--- src/practice/ui/components/practice-progress.tsx
+++ src/practice/ui/components/practice-progress.tsx
@@ -9,7 +9,7 @@ export function PracticeProgress({ progress }: PracticeProgressProps) {
     <div className="flex h-3 w-full bg-slate-50">
       <div
         className={cn(
-          "bg-primary-500 h-3 duration-500 bg-slate-500 transition-all",
+          "bg-primary-500 h-3 bg-slate-500 transition-all duration-500",
           progress !== 1 && "rounded-r-sm",
         )}
         style={{ width: `${Math.floor(progress * 100)}%` }}
