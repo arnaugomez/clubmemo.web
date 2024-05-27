ef1c0a314a90a058267b049b1dcaf9e5bcf67f64
Arnau <agomez@mrmilu.com>
2024-05-27 18:41:38 +0200

    fix: avoid circular dependency

--- src/common/di/client-locator.ts
+++ src/common/di/client-locator.ts
@@ -1,7 +1,7 @@
 import { ErrorTrackingServiceSentryImpl } from "../data/services/error-tracking-service-sentry-impl";
 import type { ErrorTrackingService } from "../domain/interfaces/error-tracking-service";
-import { singleton } from "./locator";
 import type { Dependency } from "./locator-types";
+import { singleton } from "./locator-utils";
 
 interface ClientLocator {
   ErrorTrackingService: Dependency<ErrorTrackingService>;
