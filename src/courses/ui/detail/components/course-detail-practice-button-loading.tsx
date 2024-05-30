ae9cb948b0c85ba6b2f10ecdc5ec588d51d07f0c
Arnau <agomez@mrmilu.com>
2024-05-30 21:29:44 +0200

    feat: show loading button when loading practice of course, and never show an empty space instead

---
+++ src/courses/ui/detail/components/course-detail-practice-button-loading.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/src/common/ui/components/shadcn/ui/button";
+
+export function CourseDetailPracticeButtonLoading() {
+  return (
+    <Button className="w-full" disabled>
+      Cargando práctica...
+    </Button>
+  );
+}
