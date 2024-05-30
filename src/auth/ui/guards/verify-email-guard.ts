d86b71266bd74c46f3b5e3c7ded8530102f162df
Arnau <agomez@mrmilu.com>
2024-05-30 19:38:06 +0200

    chore: format

--- src/auth/ui/guards/verify-email-guard.ts
+++ src/auth/ui/guards/verify-email-guard.ts
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { fetchSession } from "../fetch/fetch-session";
 
 export async function verifyEmailGuard() {
-  const {user} = await fetchSession();
+  const { user } = await fetchSession();
   if (user && !user.isEmailVerified) {
     redirect("/auth/verify-email");
   }
