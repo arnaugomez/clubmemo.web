899fce9dac368af030123b830ce1378708f64eac
Arnau <agomez@mrmilu.com>
2024-05-31 20:12:07 +0200

    style: format code

--- src/common/domain/interfaces/ip-service.ts
+++ src/common/domain/interfaces/ip-service.ts
@@ -3,7 +3,6 @@
  * request)
  */
 export interface IpService {
-
   /**
    * Gets the IP address of the user (i.e., the client making the request)
    * @returns The IP address of the user, or "0.0.0.0" if it could not be
