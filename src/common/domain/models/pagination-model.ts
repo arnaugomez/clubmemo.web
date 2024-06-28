c8d9784cc6cbee0075c46b3c8326abadcc4fd0d2
Arnau <agomez@mrmilu.com>
2024-06-28 14:35:56 +0200

    feat: feat: pagination model tests

--- src/common/domain/models/pagination-model.ts
+++ src/common/domain/models/pagination-model.ts
@@ -1,15 +1,29 @@
+/**
+ * Serialized data of `PaginationModel`
+ */
 export interface PaginationModelData<T> {
   results: T[];
   totalCount: number;
 }
 
+/**
+ * A paginated list. Contains a fraction of the results of a query.
+ * It also contains the total count of the results.
+ */
 export class PaginationModel<T> {
   constructor(public readonly data: PaginationModelData<T>) {}
 
+  /**
+   * Elements of the paginated list
+   */
   get results() {
     return this.data.results;
   }
 
+  /**
+   * Total count of the results of the query.
+   * It is equal or larger than the length of the results list.
+   */
   get totalCount() {
     return this.data.totalCount;
   }
