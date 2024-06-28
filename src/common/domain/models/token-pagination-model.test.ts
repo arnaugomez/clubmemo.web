7f744383ec8a6984d6db956085fed47664024df2
Arnau <agomez@mrmilu.com>
2024-06-28 14:56:18 +0200

    feat: add test to token pagination model

---
+++ src/common/domain/models/token-pagination-model.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+import { TokenPaginationModel } from "./token-pagination-model";
+
+describe("TokenPaginationModel", () => {
+  it("properties match value", () => {
+    const data = { results: [1, 2, 3], token: "test-token-134" };
+    const model = new TokenPaginationModel(data);
+    expect(model.results).toEqual(data.results);
+    expect(model.token).toEqual(data.token);
+  });
+
+  it("toData serializes data", () => {
+    const data = { results: [1, 2, 3], token: "testToken" };
+    const model = new TokenPaginationModel(data);
+    const serializedData = model.toData((num: number) => num.toString());
+    expect(serializedData).toEqual({
+      token: data.token,
+      results: ["1", "2", "3"],
+    });
+  });
+
+  it("fromData deserializes data", () => {
+    const data = { results: ["1", "2", "3"], token: "testToken" };
+    const model = TokenPaginationModel.fromData(data, (str: string) =>
+      parseInt(str),
+    );
+    expect(model.data).toEqual({ token: data.token, results: [1, 2, 3] });
+  });
+});
--- src/common/domain/models/token-pagination-model.ts
+++ src/common/domain/models/token-pagination-model.ts
@@ -1,19 +1,40 @@
+/**
+ * Data of a paginated list of items with a token to fetch the next page.
+ */
 export interface TokenPaginationModelData<T> {
   results: T[];
   token?: string;
 }
 
+/**
+ * A paginated list of items with a token to fetch the next page. Contains a
+ * portion of the results of a query and a token that identifies the last item
+ * in the list, which can be used to get the next items.
+ */
 export class TokenPaginationModel<T> {
-  constructor(readonly data: TokenPaginationModelData<T>) {}
+  constructor(private readonly data: TokenPaginationModelData<T>) {}
 
+  /**
+   * The list of items in the current page.
+   */
   get results() {
     return this.data.results;
   }
 
+  /**
+   * The identifier of the last item in the `results` list. Used as a token to
+   * fetch the next page of items.
+   */
   get token() {
     return this.data.token;
   }
 
+  /**
+   * Serializes the paginated list
+   *
+   * @param serializer Function that converts an item in the `results` list to a serializable format.
+   * @returns A serialized version of the paginated list.
+   */
   toData<U>(serializer: (data: T) => U): TokenPaginationModelData<U> {
     return {
       ...this.data,
@@ -21,6 +42,12 @@ export class TokenPaginationModel<T> {
     };
   }
 
+  /**
+   * Deserializes the paginated list
+   *
+   * @param serializer Function that converts a serialized item in the `results` list to a domain model
+   * @returns The original paginated list deserialized.
+   */
   static fromData<T, U>(
     data: TokenPaginationModelData<U>,
     deserializer: (data: U) => T,
