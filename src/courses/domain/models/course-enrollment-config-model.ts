866c0901cba369bb24903cc1d6915c38e7715649
Arnau Gómez <agomez1f@gmail.com>
2024-07-01 23:00:14 +0200

    feat: add tests

--- src/courses/domain/models/course-enrollment-config-model.ts
+++ src/courses/domain/models/course-enrollment-config-model.ts
@@ -12,9 +12,17 @@ export interface CourseEnrollmentConfigModelData {
   showAdvancedRatingOptions?: boolean;
 }
 
+/**
+ * The configuration of the practice of a course. Its parameters describe the
+ * way the user practices a course.
+ */
 export class CourseEnrollmentConfigModel {
   constructor(readonly data: CourseEnrollmentConfigModelData) {}
 
+  /**
+   * Creates an empty configuration with default parameters
+   * @returns An empty course enrollment config
+   */
   static empty() {
     return new CourseEnrollmentConfigModel({});
   }
@@ -43,6 +51,12 @@ export class CourseEnrollmentConfigModel {
     return 10;
   }
 
+  /**
+   * Parameters for the FSRS spaced repetition algorithm
+   * @returns The parameters for the FSRS spaced repetition algorithm
+   * @see FSRS
+   * @see CourseEnrollmentModel
+   */
   get fsrsGeneratorParameters() {
     return generatorParameters({
       enable_fuzz: this.enableFuzz,
@@ -51,6 +65,12 @@ export class CourseEnrollmentConfigModel {
     });
   }
 
+  /**
+   * Returns the amount of new cards left to practice today
+   *
+   * @param reviewsOfNewCardsCount Reviews of new cards done today
+   * @returns The amount of new cards that should be practiced today
+   */
   getNewCount(reviewsOfNewCardsCount: number) {
     return Math.max(0, this.dailyNewCardsCount - reviewsOfNewCardsCount);
   }
