326b123a537bb244598075e5fc8fedfc4715ebb5
Arnau Gómez <agomez1f@gmail.com>
2024-10-07 19:05:52 +0200

    feat: file table cell

---
+++ src/admin/ui/resource-list/components/file-table-cell.tsx
@@ -0,0 +1,31 @@
+import { TableCell } from "@/src/common/ui/components/shadcn/ui/table";
+import { textStyles } from "@/src/common/ui/styles/text-styles";
+import { cn } from "@/src/common/ui/utils/shadcn";
+import { ExternalLinkIcon } from "lucide-react";
+import Link from "next/link";
+
+interface FileTableCellProps {
+  href: string;
+}
+export function FileTableCell({ href }: FileTableCellProps) {
+  href = href?.trim();
+
+  function renderLink() {
+    if (!href) return null;
+    return (
+      <Link href={href} className="hover:underline">
+        Ver archivo
+        <ExternalLinkIcon
+          className="ml-2 inline-block -translate-y-px cursor-pointer hover:text-slate-900"
+          size={14}
+        />
+      </Link>
+    );
+  }
+
+  return (
+    <TableCell className={cn(textStyles.mutedLink, "whitespace-nowrap")}>
+      {renderLink() || "-"}
+    </TableCell>
+  );
+}
--- src/admin/ui/resource-list/components/resource-list-table-cell.tsx
+++ src/admin/ui/resource-list/components/resource-list-table-cell.tsx
@@ -7,6 +7,7 @@ import { AdminFieldTypeModel } from "@/src/admin/domain/models/admin-resource-mo
 import { TableCell } from "@/src/common/ui/components/shadcn/ui/table";
 import dayjs from "dayjs";
 import { translateAdminKey } from "../../i18n/admin-translations";
+import { FileTableCell } from "./file-table-cell";
 import { IdTableCell } from "./id-table-cell";
 
 interface ResourceListTableCellProps {
@@ -51,21 +52,25 @@ export function ResourceListTableCell({
     }
     return value?.toString() || "-";
   }
-  if (field.fieldType === AdminFieldTypeModel.objectId) {
-    return (
-      <IdTableCell
-        key={field.name}
-        id={data[field.name]}
-        resourceType={field.resourceType}
-      />
-    );
+  switch (field.fieldType) {
+    case AdminFieldTypeModel.objectId:
+      return (
+        <IdTableCell
+          key={field.name}
+          id={data[field.name]}
+          resourceType={field.resourceType}
+        />
+      );
+    case AdminFieldTypeModel.file:
+      return <FileTableCell href={data[field.name]} />;
+    default:
+      return (
+        <TableCell
+          className="min-w-[100px] max-w-[500px] truncate"
+          key={field.name}
+        >
+          {getStringValue()}
+        </TableCell>
+      );
   }
-  return (
-    <TableCell
-      className="min-w-[100px] max-w-[500px] truncate"
-      key={field.name}
-    >
-      {getStringValue()}
-    </TableCell>
-  );
 }
