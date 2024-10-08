0f4cc0c93b4203b42633f91e9b6e7dfdfbb2165d
Arnau Gómez <agomez1f@gmail.com>
2024-10-08 19:45:23 +0200

    fix: change password schema to allow for apple passwords

--- src/common/schemas/password-schema.ts
+++ src/common/schemas/password-schema.ts
@@ -4,12 +4,12 @@ export const PasswordSchema = z
   .string()
   .min(8)
   .max(256)
-  .refine((value) => /[a-z]/.test(value), {
-    params: { i18n: "atLeastOneLowerCase" },
-  })
-  .refine((value) => /[A-Z]/.test(value), {
-    params: { i18n: "atLeastOneUpperCase" },
+  .refine((value) => /[a-zA-Z]/.test(value), {
+    params: { i18n: "atLeastOneLetter" },
   })
   .refine((value) => /[0-9]/.test(value), {
     params: { i18n: "atLeastOneNumber" },
+  })
+  .refine((value) => /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/.test(value), {
+    params: { i18n: "atLeastOneSpecialCharacter" },
   });
