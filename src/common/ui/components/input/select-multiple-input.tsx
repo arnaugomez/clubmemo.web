3efb880e5facbaf7928812f2aae23c94c316b8e6
Arnau Gómez <agomez1f@gmail.com>
2024-10-16 09:27:30 +0200

    fix: id of selector match label

--- src/common/ui/components/input/select-multiple-input.tsx
+++ src/common/ui/components/input/select-multiple-input.tsx
@@ -24,7 +24,7 @@ export function SelectMultipleInput({
 }: SelectMultipleInputProps) {
   return (
     <Select
-      id={id}
+      inputId={id}
       placeholder={placeholder}
       value={options.filter((option) => value.includes(option.value))}
       isMulti
