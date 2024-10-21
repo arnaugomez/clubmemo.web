78d28e03a32cdf8c0b633efdc280196c93358063
Arnau Gómez <agomez1f@gmail.com>
2024-10-21 23:30:00 +0200

    docs(common): document database index generation

--- app/(navbar-layout)/(login-guard)/admin/resources/[resourceType]/page.tsx
+++ app/(navbar-layout)/(login-guard)/admin/resources/[resourceType]/page.tsx
@@ -8,11 +8,18 @@ import { locator_common_DatabaseIndexesService } from "@/src/common/locators/loc
 export const dynamicParams = false;
 
 /**
- * https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+ * Generates the possible values of the path parameters for the page.
+ *
+ * It also generates database indexes. Index generation should be done once, at
+ * build time, because doing it at runtime on every request introduces an
+ * additional overhead to the web servers and increases latency.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-static-params
  */
 export async function generateStaticParams() {
   const databaseIndexesService = locator_common_DatabaseIndexesService();
   await databaseIndexesService.createIndexes();
+
   return adminResourcesConfig.map((resource) => ({
     resourceType: resource.resourceType,
   }));
--- src/common/domain/interfaces/database-indexes-service.ts
+++ src/common/domain/interfaces/database-indexes-service.ts
@@ -1,3 +1,12 @@
+/**
+ * Service to manage database indexes. Indexes are used to speed up
+ * queries and enforce constraints. This service is responsible for
+ * the initial setup of indexes in the database.
+ */
 export interface DatabaseIndexesService {
+  /**
+   * Create all indexes in the database. Should be called once, when the
+   * application builds.
+   */
   createIndexes(): Promise<void>;
 }
