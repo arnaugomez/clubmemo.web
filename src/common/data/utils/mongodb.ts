8624943d76083827671a0539bef53d0bc455981c
Arnau Gómez <agomez1f@gmail.com>
2024-10-22 16:10:21 +0200

    feat: improve mongodb utility object

--- src/common/data/utils/mongodb.ts
+++ src/common/data/utils/mongodb.ts
@@ -1,16 +1,29 @@
 import type { Collection, Document } from "mongodb";
 
+/**
+ * Utility type that represents a MongoDB collection.
+ */
 export type CollectionType<
   TSchema extends Document,
   TName extends string = string,
 > = {
+  /**
+   * String identifier of the collection in the database
+   */
   name: TName;
+  /**
+   * Type of the MongoDB collection. Warning: use it for type inference only.
+   * Its actual value is `undefined`.
+   * @example private readonly users: typeof usersCollection.type;
+   */
   type: Collection<TSchema>;
 };
 
+/**
+ * Creates a MongoDB `CollectionType` object that identifies a database
+ * collection.
+ */
 export const collection =
   <TSchema extends Document>() =>
-  <TName extends string>(name: TName) => ({
-    name,
-    type: undefined as unknown as Collection<TSchema>,
-  });
+  <TName extends string>(name: TName) =>
+    ({ name }) as CollectionType<TSchema, TName>;
