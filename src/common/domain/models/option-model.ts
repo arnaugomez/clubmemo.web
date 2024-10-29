80800709219453040a0a39e0c1ff897a55052c77
Arnau Gómez <agomez1f@gmail.com>
2024-10-29 16:27:12 +0100

    feat: add documentation comments

--- src/common/domain/models/option-model.ts
+++ src/common/domain/models/option-model.ts
@@ -1,4 +1,15 @@
+/**
+ * Represents an option in a select input, a radio group
+ * or any other component that requires a label and a value.
+ */
 export interface OptionModel {
+  /**
+   * Represents the value in natural language, that a human can understand.
+   */
   label: string;
+  /**
+   * The value used in the application logic. For example, the value is sent to
+   * the server and stored in the database, while the label is not.
+   */
   value: string;
 }
